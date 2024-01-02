cef9bd234a202b1d073419b0d6bb3f908df775d7
raghunath23 <121800714+raghunath23@users.noreply.github.com>
2024-01-02 16:00:55 +0530

    Update lab8.py

--- lab8.py
+++ lab8.py
@@ -37,3 +37,12 @@ if __name__ == "__main__":
             print(f"{vertex}: {color}")
     else:
         print("No solution found.")
+
+"""
+Sample output
+Solution found:
+A: Red
+B: Green
+C: Blue
+D: Red
+"""
